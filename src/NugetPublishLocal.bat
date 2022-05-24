131a8ed556d6e510e9c439f7a48547961e4c2e31
Sergey Ratkevich <ratkevich@syntellect.ru>
2022-05-24 11:53:41 +0300

    Line endings and project configuration.

--- src/NugetPublishLocal.bat
+++ src/NugetPublishLocal.bat
@@ -3,4 +3,5 @@ del *.nupkg
 set NUGET=.\.nuget\nuget.exe
 set LocalSource="C:\workspace\nugetLocal"
 rem %NUGET% pack .\Winium.Cruciatus\Winium.Cruciatus.csproj -IncludeReferencedProjects -Prop Configuration=Debug
-%NUGET% push .\Winium.Cruciatus\bin\Debug\*.nupkg -Source %LocalSource%
+rem %NUGET% push .\Winium.Cruciatus\bin\Debug\*.nupkg -Source %LocalSource%
+%NUGET% push .\Winium.Cruciatus\bin\Release\*.nupkg -Source %LocalSource%
